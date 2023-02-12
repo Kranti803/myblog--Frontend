4b48e38a11847631ca9f5a65e6b6eea9261532e4
Kranti Kumar Shah <Krantikumar803@gmail.com>
2023-02-12 16:30:52 +0545

    updated mediaquery and resetpassword page and forgot password page

--- src/components/Auth/ForgotPassword/ForgotPassword.jsx
+++ src/components/Auth/ForgotPassword/ForgotPassword.jsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import { toast } from "react-hot-toast";
-import { clearError, clearMessage } from "../../../redux/reducers/userSlice";
 import { forgotPassword } from "../../../redux/actions/userActions";
 
 const ForgotPassword = () => {
@@ -10,7 +7,6 @@ const ForgotPassword = () => {
 
   const dispatch = useDispatch();
 
-  const { error, message } = useSelector((state) => state.user);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -18,16 +14,7 @@ const ForgotPassword = () => {
     dispatch(forgotPassword(email));
   };
 
-  useEffect(() => {
-    if (error) {
-      toast.error(error);
-      dispatch(clearError());
-    }
-    if (message) {
-      toast.success(message);
-      dispatch(clearMessage());
-    }
-  }, [dispatch, error, message]);
+
 
   return (
     <section className="forgotpassword">
--- src/components/Auth/ResetPassword/ResetPassword.jsx
+++ src/components/Auth/ResetPassword/ResetPassword.jsx
@@ -1,38 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { resetPassword } from "../../../redux/actions/userActions";
-import { toast } from "react-hot-toast";
-import { clearError, clearMessage } from "../../../redux/reducers/userSlice";
 
 const ResetPassword = () => {
   const [password, setPassword] = useState("");
 
   const { token } = useParams();
-  console.log(token);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { error, message } = useSelector((state) => state.user);
 
-  const submitHandler = (e) => {
+  const submitHandler = async(e) => {
     e.preventDefault();
 
-    dispatch(resetPassword(password, token));
+    await dispatch(resetPassword(password, token));
+    navigate('/login')
   };
-  useEffect(() => {
-    if (error) {
-      toast.error(error);
-      dispatch(clearError());
-    }
-    if (message) {
-      toast.success(message);
-      dispatch(clearMessage());
-      navigate("/login");
-    }
-  }, [dispatch, error, message, navigate]);
+
 
   return (
     <section className="resetpassword">
